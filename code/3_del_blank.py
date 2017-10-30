637a5830ed63014b006b9876be1d187b03c3e168
i0Ek3 <kno30826@gmail.com>
2017-10-30 16:58:31 +0800

    update something

---
+++ code/3_del_blank.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+temp1 = ' python'
+temp2 = 'python '
+print("The origin char is:")
+print(temp1)
+print(temp2)
+
+t1 = temp1.rstrip()
+t2 = temp1.rstrip()
+print("The newest char is:")
+print(t1)
+print(t2)
+
