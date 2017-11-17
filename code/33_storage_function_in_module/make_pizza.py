d7540a1213d858c2efbae7255a24aabda9a5b19d
i0Ek3 <kno30826@gmail.com>
2017-11-17 19:12:02 +0800

    add 33_storage_function_in_module/

---
+++ code/33_storage_function_in_module/make_pizza.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def make_pizza(size,*toppings):
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("--> " + topping)
---
+++ code/33_storage_function_in_module/module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+#导入特定的函数
+#from module_name import function_name
+#from module_name import function0,func...
+
+from make_pizza import make_pizza
+
+make_pizza(23,'piglet')
+make_pizza(53,'piglet','pepper','glas')
+
+
