b94ed441dffbd02bb56e038b3f8f1becd92cb993
i0Ek3 <kno30826@gmail.com>
2017-12-04 10:19:39 +0800

    update admin.py

--- code/part2/proj3/learning_logs/admin.py
+++ code/part2/proj3/learning_logs/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from leatning_logs.models import Topic # import the register model we needed
+from learning_logs.models import Topic # import the register model we needed
 
 admin.site.register(Topic) # use Django manage our model by manage website
