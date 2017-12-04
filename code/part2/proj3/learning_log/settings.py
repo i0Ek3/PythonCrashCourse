55a8309176d234f120dda9529b72c622cca290c0
i0Ek3 <kno30826@gmail.com>
2017-12-04 09:17:38 +0800

    add application

--- code/part2/proj3/learning_log/settings.py
+++ code/part2/proj3/learning_log/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my application
+    'learning_logs',
+
 ]
 
 MIDDLEWARE = [
