78ee49cf2eae5587e52a3d50d360151809311f51
i0Ek3 <kno30826@gmail.com>
2017-12-06 13:57:11 +0800

    revised urls.py

--- code/part2/proj3/learning_logs/urls.py
+++ code/part2/proj3/learning_logs/urls.py
@@ -16,4 +16,8 @@ from . import views
 urlpatterns = [
         # mainpage
         url(r'^$',views.index,name='index'),
+        
+        # show all topics
+        url(r'^topics/$',views.topics,name='topics'),
+
         ]
