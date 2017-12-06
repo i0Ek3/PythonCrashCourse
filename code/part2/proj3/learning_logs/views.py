17b26f1387da6a86e393673044b260b7e0dba730
i0Ek3 <kno30826@gmail.com>
2017-12-06 14:01:58 +0800

    import topic

--- code/part2/proj3/learning_logs/views.py
+++ code/part2/proj3/learning_logs/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topic
 # Create your views here.
 
 def index(request):
     """Mainpage of Learning_logs"""
 
     return render(request,'learning_logs/index.html')
+
+def topics(request):
+    """show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request,'learning_logs/topics.html',context)
