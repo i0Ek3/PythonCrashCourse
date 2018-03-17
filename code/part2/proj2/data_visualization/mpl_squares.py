d7163c0e41d180dadf53e78c1d3449ff5992821c
i0Ek3 <kno30826@gmail.com>
2018-03-17 15:42:33 +0800

    revised chart

--- code/part2/proj2/data_visualization/mpl_squares.py
+++ code/part2/proj2/data_visualization/mpl_squares.py
@@ -9,8 +9,10 @@
 
 import matplotlib.pyplot as plt
 
+# revise chart
+input_values=[1,2,3,4,5]
 squares = [1,4,9,16,25]
-plt.plot(squares,linewidth=5)
+plt.plot(input_values,squares,linewidth=5)
 
 # set chart title
 plt.title("Square Numbers",fontsize=24)
