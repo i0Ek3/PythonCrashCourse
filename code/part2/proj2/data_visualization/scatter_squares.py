ee4f518f6fee0f0d8994cc609bed0e3c1e916523
i0Ek3 <kno30826@gmail.com>
2018-03-18 11:05:48 +0800

    add scatter

---
+++ code/part2/proj2/data_visualization/scatter_squares.py
@@ -0,0 +1,30 @@
+## 
+## scatter_squares.py
+## @ianpasm(kno30826@gmail.com)
+## 2018-03-18 10:34:38
+## 
+ 
+#!/usr/bin/env python3
+# coding=utf-8
+
+import matplotlib.pyplot as plt
+
+#x_values = [1,2,3,4,5]
+#y_values = [1,4,9,16,25]
+
+x_values = list(range(1,1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values,y_values,c=y_values,cmap=plt.cm.Blues,edgecolor='none',s=40)
+
+plt.scatter(2,4,s=200)
+plt.title("Squares Numbers",fontsize=24)
+plt.xlabel("Value",fontsize=14)
+plt.ylabel("Square of Value",fontsize=14)
+
+plt.tick_params(axis='both',which='major',labelsize=14)
+
+plt.axis([0,1100,0,1100000])
+
+#plt.savefig('squares_plot.png',bbox_inches='tight')
+plt.show()
