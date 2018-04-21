9b3a1bfed0f4392b8658ca4f7537d114cd84970b
i0Ek3 <kno30826@gmail.com>
2018-04-21 19:33:00 +0800

    create windows and response user input

---
+++ code/part2/proj1/alien_invasion/alien_invasion.py
@@ -0,0 +1,26 @@
+## 
+## alien_invasion.py
+## @ianpasm(kno30826@gmail.com)
+## 2018-04-21 19:28:46
+## 
+ 
+#!/usr/bin/env python3
+# coding=utf-8
+
+import sys
+import pygame
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    while Ture:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
+run_game()
+
