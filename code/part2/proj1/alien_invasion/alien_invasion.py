7e8ac658711189f283060a16221399b52782c6b3
i0Ek3 <kno30826@gmail.com>
2018-04-21 19:48:52 +0800

    fixed error

--- code/part2/proj1/alien_invasion/alien_invasion.py
+++ code/part2/proj1/alien_invasion/alien_invasion.py
@@ -15,7 +15,7 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
-    while Ture:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
