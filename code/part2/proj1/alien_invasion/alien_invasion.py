6f39f7282da24069e8db29f7b2d25561ae93954f
i0Ek3 <kno30826@gmail.com>
2018-04-23 21:29:44 +0800

    add backgroud color code

--- code/part2/proj1/alien_invasion/alien_invasion.py
+++ code/part2/proj1/alien_invasion/alien_invasion.py
@@ -12,15 +12,19 @@ import pygame
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((1200, 800)) #create window, screen use to show element of the game
     pygame.display.set_caption("Alien Invasion")
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+    #setting backgroud color
+    bg_color = (230, 230, 230)
 
-        pygame.display.flip()
+    while True: #loop to update code on the screen
+        for event in pygame.event.get(): #visit event
+            if event.type == pygame.QUIT:
+                sys.exit() #exit the game
+
+        screen.fill(bg_color) #every time to loop to update screen, color defined by RGB range in 0~255.
+        pygame.display.flip() #make the screen visible which pictured on nearly
 
 run_game()
 
