ae5f24f9e7b21f4756f97e6d909b62e9363eb266
i0Ek3 <kno30826@gmail.com>
2018-04-30 14:32:06 +0800

    add airship

--- code/part2/proj1/alien_invasion/alien_invasion.py
+++ code/part2/proj1/alien_invasion/alien_invasion.py
@@ -10,7 +10,7 @@
 import sys
 import pygame
 from settings import Settings
-
+from ship import Ship
 
 def run_game():
     pygame.init()
@@ -22,12 +22,17 @@ def run_game():
     #setting backgroud color
     bg_color = (230, 230, 230)
 
+    # create an airship
+    ship = Ship(screen)
+
     while True: #loop to update code on the screen
         for event in pygame.event.get(): #visit event
             if event.type == pygame.QUIT:
                 sys.exit() #exit the game
 
         screen.fill(ai_settings.bg_color) #every time to loop to update screen, color defined by RGB range in 0~255.
+        ship.blitme()
+        
         pygame.display.flip() #make the screen visible which pictured on nearly
 
 run_game()
